5f693d2b0e269c4bbdcda7281a82a227ddcbe57e
Rico <frankiewb9527@gmail.com>
2022-08-22 23:18:45 +0800

    添加 TDesign

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import TDesign from 'tdesign-vue-next'
+import 'tdesign-vue-next/es/style/index.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(TDesign)
 
 app.mount('#app')
