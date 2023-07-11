4a21415e9151adc7e8f3318c6b7f0c1c3796b75d
Niyaco <frankiewb9527@gmail.com>
2023-07-11 23:47:31 +0800

    chore: add vite-plugin-vue-devtools

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import VueJsx from '@vitejs/plugin-vue-jsx'
 import VueRouter from 'unplugin-vue-router/vite'
+import VueDevTools from 'vite-plugin-vue-devtools'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
@@ -17,6 +18,7 @@ export default defineConfig({
     }),
     Vue(),
     VueJsx(),
+    VueDevTools(),
     AutoImport({
       dts: true,
       imports: [
